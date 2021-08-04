e184eee89fc8aab87e15c29507d42bfe4626db30
Cody Anderson <anderscody09@gmail.com>
2021-08-04 12:55:12 -0500

    initial login refactor push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Login from './components/login-component/login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <NavComponent></NavComponent>
+        <Container>
+          <Switch>
+            <Route path="/login" component={Login}>
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
   );
 }
 
