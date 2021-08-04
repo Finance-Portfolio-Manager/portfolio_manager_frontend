48ac588d1594fb019ed53ef80a897d3c52bd72fe
Cody Anderson <anderscody09@gmail.com>
2021-08-04 14:40:27 -0500

    working login refactor w/ fetch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Login from './components/login-component/login';
-=======
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Portfolio from './components/portfolio-component/portfolio-component';
-import Home from './components/home-component/home-component';
-import NewTransaction from './components/new-transaction-component/new-transaction-component';
-import Login from './components/login-component/login-component';
-import Register from './components/register-component/register-component';
->>>>>>> df4799115c5be3570cc545666e53ef09e051c141
 
 //TODO: We either need to add the navbar to this page or add it as a separate component
 
@@ -20,28 +11,14 @@ import Register from './components/register-component/register-component';
 //TODO: All of these components are just placeholders for different groups now
 function App() {
   return (
-<<<<<<< HEAD
-      <Router>
-        <NavComponent></NavComponent>
-        <Container>
-          <Switch>
-            <Route path="/login" component={Login}>
-            </Route>
-          </Switch>
-        </Container>
-      </Router>
-  );
-=======
     <Router>
-      <Route path="/" exact component={Home}/>
+      {/* <Route path="/" exact component={Home}/>
       <Route path="/new" component={NewTransaction}/>
-      <Route path="/portfolio" component={Portfolio}/>
+      <Route path="/portfolio" component={Portfolio}/> */}
       <Route path="/login" component={Login}/>
-      <Route path="/register" component={Register}/>
+      {/* <Route path="/register" component={Register}/> */}
     </Router>
     );
-
->>>>>>> df4799115c5be3570cc545666e53ef09e051c141
 }
 
 export default App;
--- src/components/login-component/login.js
+++ src/components/login-component/login.js
@@ -1,29 +1,66 @@
 import axios from "axios";
 import { useState } from "react";
+import { withRouter } from "react-router-dom";
+import LoginForm from "./login-form";
 
 
-function LogIn(props){
+
+function Login(props){
 
     const [credentials, setCredentials] = useState("");
 
     const handleChange = (e) =>{
         const {value,name} = e.target;
-
+        
         setCredentials({...credentials, [name]:value});
+        console.log(credentials);
     }
 
     const handleSubmit = (e)=>{
         e.preventDefault();
-        const requestBody = 'username:${credentials.username},password:${credentials.password}';
-        console.log(JSON.stringify(requestBody));
-        axios.post("http://localhost:8082", JSON.stringify(requestBody), {headers:{"Content Type": "application/json"}})
-            then(response=>{
-                console.log("success");
-            })
-            .catch(err=> console.error("error"))
-    }
 
-    return <LoginForm></LoginForm>
+        const loginInfo = {
+                username:`${credentials.username}`,
+                password:`${credentials.password}`
+            };
+        console.log(JSON.stringify(loginInfo));
+
+        fetch("http://localhost:8082/login", {
+        method: 'post',
+        headers: new Headers({
+            'Content-Type':'application/json'
+        }),
+        body: JSON.stringify(loginInfo)
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+                console.log("here");
+                console.log(data.jwt)
+                sessionStorage.setItem("Authorization", data.jwt)
+                if(data.jwt==undefined){
+
+                } else if (data.jwt != null && data.jwt != undefined){
+                    console.log("test0");
+                 }  
+            })
+    .catch((error) => {
+
+    })
+}
+
+
+        // axios.post("localhost:8082/login", JSON.stringify(requestBody), {headers:{"Content-Type": "application/json"}})
+        //     .then(response=>{
+        //         console.log("successful login!!!");
+        //         // sessionStorage.setItem("token", response.headers["authorization"]);
+        //         // props.history.push("/");
+        //     })
+        //     .catch(err=> console.error("there was an issue logging in"))
+    // }
+
+
+
+    return <LoginForm onChange={handleChange} onSubmit={handleSubmit}></LoginForm>;
 }
 
 export default withRouter(Login);
