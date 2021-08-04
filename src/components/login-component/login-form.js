947a3f620f606c9fd687c1376d47ee8cb46db0f5
Cody Anderson <anderscody09@gmail.com>
2021-08-04 18:09:39 -0500

    login form css

--- src/components/login-component/login-form.js
+++ src/components/login-component/login-form.js
@@ -1,16 +1,29 @@
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import './login.css';
+import logo from './revlogo.jpg';
 
 export default function LoginForm(props){
     return(
-        <Form>
-            <Form.Group>
-                <Form.Label>Username:</Form.Label>
-                <Form.Control name="username" placeholder="Enter your username" onChange={props.onChange}></Form.Control>
-                <Form.Label>Password:</Form.Label>
-                <Form.Control name="password" placeholder="Enter your password" onChange={props.onChange}></Form.Control>
-            </Form.Group>
-            <Button variant="primary" type="submit" onClick={props.onSubmit}>Login</Button>
-        </Form>
+        <div className="App">
+            <Form>
+                <div className="aroundLogo"><img className="logo" src={logo}/></div>
+                <div className="form-inner">
+                {/* <div> */}
+                    
+                {/* </div> */}
+                <h4>Portfolio Manager</h4>
+                <Form.Group className="form-group" controlId="formBasicUsername">
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control className="loginInput" placeholder="Enter your username" onChange={props.onChange}></Form.Control>
+                </Form.Group>
+                <Form.Group className="form-group" controlId="formBasicPassword">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control type="password" className="loginInput" placeholder="Enter your password" onChange={props.onChange}></Form.Control>
+                </Form.Group>
+                <button className="submitButton mt-2" type="submit" onClick={props.onSubmit}>Login</button>
+                </div>
+            </Form>
+    </div>
     )
 }
