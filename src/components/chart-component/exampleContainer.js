251381e7ce78437a1956a59bcf513ae803b7d668
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-05 11:12:46 -0700

    mostly working

--- src/components/chart-component/exampleContainer.js
+++ src/components/chart-component/exampleContainer.js
@@ -1,49 +1,28 @@
-import React from "react";
-import ExampleCandle from "./candlestickChart";
+import CandlestickChart from "./candlestickChart";
+import React, { useState } from "react";
+import axios from "axios";
 
 import exampleData from "../../exampleData/example-AAPL-daily.json";
-import CandlestickChart from "./candlestickChart";
 
 function ExampleContainer(props) {
 
-    // const [state, setState] = useState({
-    //     series: [{
-    //         data: convertRawData(exampleData)
-    //     }]
-    // });
+    const [state, setState] = useState({
+        rawData: exampleData
+    });
 
-    // const [state, setState] = useState({});
-    // axios.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=JPGYN8LH3C332EGE")
-    // .then(response => {
-    //     return response.data;
-    // }).then(rawData => {
-    //     setState({
-    //         options: {
-    //             chart: {
-    //                 type: 'candlestick',
-    //                 height: 350
-    //             },
-    //             title: {
-    //                 text: 'CandleStick Chart',
-    //                 align: 'left'
-    //             },
-    //             xaxis: {
-    //                 type: 'datetime'
-    //             },
-    //             yaxis: {
-    //                 tooltip: {
-    //                     enabled: true
-    //                 }
-    //             }
-    //         },
-    //         series: [{
-    //             data: convertRawData(rawData)
-    //         }]
-    //     })
-    // })
+    axios.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=JPGYN8LH3C332EGE")
+    .then(response => {
+        console.log(
+            response.data
+        );
+
+        setState({ 
+            rawData: response.data
+        })
+    })
 
     return (
-        <CandlestickChart rawData={exampleData}></CandlestickChart>
+        <CandlestickChart rawData={state.rawData}></CandlestickChart>
     )
 
 }
