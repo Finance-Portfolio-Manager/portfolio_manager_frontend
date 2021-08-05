0abc9e6268acaf3451bba77f6cd9101da4420dd0
Cody Anderson <anderscody09@gmail.com>
2021-08-04 21:18:34 -0500

    login form error handling

--- src/components/login-component/login-form.js
+++ src/components/login-component/login-form.js
@@ -9,9 +9,6 @@ export default function LoginForm(props){
             <Form>
                 <div className="aroundLogo"><img className="logo" src={logo}/></div>
                 <div className="form-inner">
-                {/* <div> */}
-                    
-                {/* </div> */}
                 <h4>Portfolio Manager</h4>
                 <Form.Group className="form-group" controlId="formBasicUsername">
                     <Form.Label>Username:</Form.Label>
@@ -22,6 +19,8 @@ export default function LoginForm(props){
                     <Form.Control type="password" className="loginInput" placeholder="Enter your password" onChange={props.onChange}></Form.Control>
                 </Form.Group>
                 <button className="submitButton mt-2" type="submit" onClick={props.onSubmit}>Login</button>
+                {props.error ? (<div id="wrongCredentials" className="form-group inner-form mt-2 mb-0 text-danger">Invalid username or password, please try again.</div>) : null}
+                {props.networkError ? (<div id="networkIssue" className="form-group inner-form mt-3 text-danger">Invalid username or password, please try again.</div>) : null}
                 </div>
             </Form>
     </div>
--- src/components/login-component/login.js
+++ src/components/login-component/login.js
@@ -3,17 +3,19 @@ import { useState } from "react";
 import { withRouter } from "react-router-dom";
 import LoginForm from "./login-form";
 
-
-
 function Login(props){
 
     const [credentials, setCredentials] = useState("");
+
+    const [error, setError] = useState(false);
+    const [networkError, setNetworkError] = useState(false);
 
     const handleChange = (e) =>{
         const {value,name} = e.target;
         
         setCredentials({...credentials, [name]:value});
-        console.log(credentials);
+        setError(false);
+        setNetworkError(false);
     }
 
     const handleSubmit = (e)=>{
@@ -30,68 +32,16 @@ function Login(props){
                 }
                 console.log(response.data.jwt);
                 sessionStorage.setItem("Authorization", response.data.jwt);
-                // props.history.push("/");
+                // props.history.push("/"); to portfolio?
             })
-            .catch(err=> console.error("There was an issue logging in!"))
+            .catch(function (error) {
+                if (error.response.status == 500) {
+                    setError(true);
+                } else {
+                    setNetworkError(true);
+                }
+            })
     }
-    return <LoginForm onChange={handleChange} onSubmit={handleSubmit}></LoginForm>;
+    return <LoginForm networkError={networkError} error={error} onChange={handleChange} onSubmit={handleSubmit}></LoginForm>;
 }
 export default withRouter(Login);
-
-
-// document.getElementById("login-form").addEventListener("submit", function(login){
-//     login.preventDefault();
-//     const userError = document.getElementById("error-credentials");
-//     const networkError = document.getElementById("error-network");
-//     userError.hidden = true;
-//     networkError.hidden = true;
-
-//     const credentials = {
-//     username:document.getElementById("inputUsername").value,
-//     password:document.getElementById("inputPassword").value
-// };
-  
-//     fetch("http://portfoliomanager-env.eba-49pyjjuv.us-east-2.elasticbeanstalk.com/login", {
-//         method: 'post',
-//         headers: new Headers({
-//             'Content-Type':'application/json'
-//         }),
-//         body: JSON.stringify(credentials)
-//     }).then((response) => {
-//         return response.json();
-//     }).then((data) => {
-//         sessionStorage.setItem("Authorization", data.jwt)
-//         if(data.jwt==undefined){
-//             userError.hidden = false;
-//         } else if (data.jwt != null && data.jwt != undefined){
-//             document.getElementById("toggle-nav-1").hidden = true;
-//             document.getElementById("toggle-nav-2").hidden = true;
-//             window.location.href = "#/portfolio";
-//             location.reload();
-//          }  
-//     }).catch((error) => {
-//             networkError.hidden = false;
-//     })
-// });
-
-    //     fetch("http://localhost:8082/login", {
-    //     method: 'post',
-    //     headers: new Headers({
-    //         'Content-Type':'application/json'
-    //     }),
-    //     body: JSON.stringify(loginInfo)
-    // }).then((response) => {
-    //     return response.json();
-    // }).then((data) => {
-    //             console.log("here");
-    //             console.log(data.jwt)
-    //             sessionStorage.setItem("Authorization", data.jwt)
-    //             if(data.jwt==undefined){
-
-    //             } else if (data.jwt != null && data.jwt != undefined){
-    //                 console.log("test0");
-    //              }  
-    //         })
-    // .catch((error) => {
-
-    // })
