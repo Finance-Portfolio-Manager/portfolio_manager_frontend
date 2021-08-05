d234c15096773f8f6ed0d09f108a098c5c38725e
Conor Scalf <c.scalf447@gmail.com>
2021-08-04 22:35:36 -0400

    Updated some styling; shrank the form a little, rounded borders, added gradient background, added subtitle

--- src/components/register-component/register-form.js
+++ src/components/register-component/register-form.js
@@ -1,5 +1,5 @@
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container,Row,Col,Button,Form } from "react-bootstrap";
 import "./register.css";
 import logo from "./revlogo.jpg";
 
@@ -10,38 +10,42 @@ export default function RegisterForm(props){
                 <div className = "aroundLogo">
                     <img className = "logo" src = {logo}/>
                 </div>
-                <h4>Portfolio Manager</h4>
-                <Form.Group className = "form-group" controlId="form-fname">
-                    <Form.Label>First Name:</Form.Label>
-                    <Form.Control name="firstName" placeholder="Enter your first name" onChange={props.onChange}/>
-                </Form.Group>
+                
+                <div className = "form-inner">
+                    <h4>Portfolio Manager</h4>
+                    <h7><em>Account Registration</em></h7>
+                    <Form.Group controlId="form-fname">
+                        <Form.Label>First Name:</Form.Label>
+                        <Form.Control name="firstName" placeholder="Enter your first name" onChange={props.onChange}/>
+                    </Form.Group>
 
-                <Form.Group className = "form-group" controlId="form-lname">
-                    <Form.Label>Last Name:</Form.Label>
-                    <Form.Control name="lastName" placeholder="Enter your last name" onChange={props.onChange}/>
-                </Form.Group>
+                    <Form.Group className = "form-group" controlId="form-lname">
+                        <Form.Label>Last Name:</Form.Label>
+                        <Form.Control name="lastName" placeholder="Enter your last name" onChange={props.onChange}/>
+                    </Form.Group>
 
-                <Form.Group className = "form-group" controlId="form-email">
-                    <Form.Label>Email:</Form.Label>
-                    <Form.Control name="email" placeholder="Enter your email" onChange={props.onChange}/>
-                </Form.Group>
+                    <Form.Group className = "form-group" controlId="form-email">
+                        <Form.Label>Email:</Form.Label>
+                        <Form.Control name="email" placeholder="Enter your email" onChange={props.onChange}/>
+                    </Form.Group>
 
-                <Form.Group className = "form-group" controlId="form-username">
-                    <Form.Label>Username:</Form.Label>
-                    <Form.Control name="username" placeholder="Enter your username" onChange={props.onChange}/>
-                </Form.Group>
+                    <Form.Group className = "form-group" controlId="form-username">
+                        <Form.Label>Username:</Form.Label>
+                        <Form.Control name="username" placeholder="Enter your username" onChange={props.onChange}/>
+                    </Form.Group>
 
-                <Form.Group className = "form-group" controlId="form-password">
-                    <Form.Label>Password:</Form.Label>
-                    <Form.Control name="password" placeholder="Enter your password" onChange={props.onChange}/>
-                </Form.Group>
+                    <Form.Group className = "form-group" controlId="form-password">
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control name="password" placeholder="Enter your password" onChange={props.onChange}/>
+                    </Form.Group>
 
-                <Form.Group className = "form-group" controlId="form-confirm-password">
-                    <Form.Label>Confirm Password:</Form.Label>
-                    <Form.Control name="confirmpassword" placeholder="Enter your password" onChange={props.onChange}/>  
-                </Form.Group>
+                    <Form.Group className = "form-group" controlId="form-confirm-password">
+                        <Form.Label>Confirm Password:</Form.Label>
+                        <Form.Control name="confirmpassword" placeholder="Enter your password" onChange={props.onChange}/>  
+                    </Form.Group>
 
-                <button className = "submitButton" type="submit" onClick={props.onSubmit}>Register</button>
+                    <button className = "submitButton mt-2" type="submit" onClick={props.onSubmit}>Register</button>
+                </div>
             </Form>
         </div>
     )
