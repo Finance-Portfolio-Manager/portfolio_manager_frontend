b57e51c12cd603e01853bdeb498edfd5177246b1
apurvp8 <apurv.patel@revature.net>
2021-08-05 11:20:02 -0400

    Team1: Portfolio Detail Page

---
+++ src/components/portfolio-component/portfolio-view-component.js
@@ -0,0 +1,55 @@
+import { Table } from "react-bootstrap";
+import { useState } from "react";
+
+export default function PortfolioView(props){
+
+    const [tableRows, setTableRows] = useState(props.tableRows);
+    const [portfolioTotal, setPortfolioTotal] = useState("");
+    const [changeValue, setChangeValue] = useState("");
+    const [changeAmount, setChangeAmount] = useState("");
+
+    //const displayRows = tableRows.map((tableRow) => tableRow);
+    // console.log(`display Rows: ${displayRows}`);
+
+    console.log(`TableRows: ${JSON.stringify(tableRows)}`);
+
+    return <>
+        <div className="wrapper fadeInDown center">
+            <div id="folioContent">
+                <div id="portfolio">
+                    <div id="portfolio-values">
+                        <h4 id = "portfolio-total"></h4>
+                        <div id = "portfolio-change"></div>
+                        <div id = "dollar-change"></div>
+                    </div>
+                    <div className="table-responsive">
+                        <Table id="stocks-view" className="table borderless table-hover" responsive>
+                            <thead>
+                                <tr>
+                                    <th scope="col">Stock</th>
+                                    <th scope="col">Quantity</th>
+                                    <th scope="col">Average Buy Price</th>
+                                    <th scope="col">Current Price</th>
+                                    <th scope="col">P/L</th>
+                                </tr>
+                            </thead>
+                            <tbody id="stocks-body">
+                                
+                                {tableRows.map((tableRow) => {
+                                    console.log(`tableRow: ${JSON.stringify(tableRow)}`);
+                                    return (<tr>
+                                        <td>{tableRow.stockName}</td>
+                                        <td>{tableRow.stockQuantity}</td>
+                                        <td>{tableRow.stockAveragePrice}</td>
+                                        <td>{tableRow.stockCurrentPrice}</td>
+                                        <td>{tableRow.stockChange}</td>
+                                    </tr>)
+                                })}
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
+            </div>
+        </div>   
+    </>
+}
