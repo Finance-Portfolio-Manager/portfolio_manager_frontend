34bbf2b40ccdb48fa6c68b468832094597703226
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-06 02:21:30 -0700

    comment

--- src/components/chart-component/chart-component.js
+++ src/components/chart-component/chart-component.js
@@ -8,17 +8,18 @@ function ChartComponent(props) {
     const [state, setState] = useState({
         symbolName: "Loading...",
         seriesData: initialSeriesData(new Date(1538856000000), 100)
-    })
+    });
 
     useEffect(()=>{
         axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${props.symbol}&apikey=JPGYN8LH3C332EGE`)
         .then(response => {
+            // put logic for multiple time frames here
             setState({
                 symbolName: response.data["Meta Data"]["2. Symbol"],
                 seriesData: convertSeriesData(response.data["Time Series (Daily)"])
             });
         })
-    },[])
+    },[]);
 
     return (
         <CandlestickChart 
@@ -26,7 +27,7 @@ function ChartComponent(props) {
             seriesData={state.seriesData}
             width={props.width}
         ></CandlestickChart>
-    )
+    );
 
 }
 
