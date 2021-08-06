158cccec81c1887b9eae411b681c1d3f1854d771
millerkalvin <Millerkalvin@gmail.com>
2021-08-06 02:08:57 -0400

    Made portfolioview component a bit more concise

--- src/components/portfolio-component/portfolio-view-component.js
+++ src/components/portfolio-component/portfolio-view-component.js
@@ -4,6 +4,7 @@ import { Button } from "react-bootstrap";
 
 export default function PortfolioView(props){
 
+    //TODO: There's no real point in tableRows being a state object if it never changes. Determine if it will change.
     const [tableRows, setTableRows] = useState(props.tableRows);
     const [portfolioTotal, setPortfolioTotal] = useState("");
     const [changeValue, setChangeValue] = useState("");
@@ -15,42 +16,38 @@ export default function PortfolioView(props){
     console.log(`TableRows: ${JSON.stringify(tableRows)}`);
 
     return <>
-        <div className="wrapper fadeInDown">
-            <div id="folioContent">
-                <div id="portfolio">
-                    <div id="portfolio-values">
-                        <h4 id = "portfolio-total"></h4>
-                        <div id = "portfolio-change"></div>
-                        <div id = "dollar-change"></div>
-                    </div>
-                    <div className="table-responsive">
-                        <Table id="stocks-view" className="table borderless table-hover" responsive>
-                            <thead>
-                                <tr>
-                                    <th scope="col">Stock</th>
-                                    <th scope="col">Quantity</th>
-                                    <th scope="col">Average Buy Price</th>
-                                    <th scope="col">Current Price</th>
-                                    <th scope="col">P/L</th>
-                                </tr>
-                            </thead>
-                            <tbody id="stocks-body">
-                                
-                                {tableRows.map((tableRow) => {
-                                    console.log(`tableRow: ${JSON.stringify(tableRow)}`);
-                                    return (<tr>
-                                        <td>{tableRow.stockName}</td>
-                                        <td>{tableRow.stockQuantity}</td>
-                                        <td>{tableRow.stockAveragePrice}</td>
-                                        <td>{tableRow.stockCurrentPrice}</td>
-                                        <td>{tableRow.stockChange}</td>
-                                    </tr>)
-                                })}
-                            </tbody>
-                        </Table>
-                        <Button variant="primary" onClick={props.toggleShowDetails}>Go to card</Button>
-                    </div>
-                </div>
+        <div className="wrapper fadeInDown folioContent" id="portfolio">
+            <div id="portfolio-values">
+                <h4 id = "portfolio-total"></h4>
+                <div id = "portfolio-change"></div>
+                <div id = "dollar-change"></div>
+            </div>
+            <div className="table-responsive">
+                <Table id="stocks-view" className="table borderless table-hover" responsive>
+                    <thead>
+                        <tr>
+                            <th scope="col">Stock</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Average Buy Price</th>
+                            <th scope="col">Current Price</th>
+                            <th scope="col">P/L</th>
+                        </tr>
+                    </thead>
+                    <tbody id="stocks-body">
+                        
+                        {tableRows.map((tableRow) => {
+                            console.log(`tableRow: ${JSON.stringify(tableRow)}`);
+                            return (<tr>
+                                <td>{tableRow.stockName}</td>
+                                <td>{tableRow.stockQuantity}</td>
+                                <td>{tableRow.stockAveragePrice}</td>
+                                <td>{tableRow.stockCurrentPrice}</td>
+                                <td>{tableRow.stockChange}</td>
+                            </tr>)
+                        })}
+                    </tbody>
+                </Table>
+                <Button variant="primary" onClick={props.toggleShowDetails}>Go to card</Button>
             </div>
         </div>   
     </>
