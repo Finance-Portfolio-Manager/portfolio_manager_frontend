d1318294f4efd44d09c0caaae34534173a3a165e
Dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2021-08-06 02:20:15 -0400

    committing useState and useEffect in scrolling strip container

--- src/components/header-component/scrolling-strip-container.js
+++ src/components/header-component/scrolling-strip-container.js
@@ -1,7 +1,11 @@
 // Author: David Garcia
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PresentScrollingStrip from "./scrolling-strip-presentation";
+import axios from "axios";
+
+const url = "http://23.22.140.95:8082";
+
 
 export default function ScrollingStripContainer(){
     var text = "⬆ ⬇ Just relax and let it flow. That easy. You could sit here for weeks with your one hair brush trying to do that - or you could do it with one stroke with an almighty brush. We can fix anything. It just happens - whether or not you worried about it or tried to plan it. How to paint. That's easy. What to paint. That's much harder. Don't fiddle with it all day.";
@@ -10,6 +14,12 @@ export default function ScrollingStripContainer(){
         { symbol: "AMZN" , PNL: -5 },
         { symbol: "GOOG" , PNL: +5}
     ]
+
+    const [stockJson, setStockJson] = useState({});
+
+    useEffect(()=> {
+        getStockById(1,setStockJson);
+    },[]);
         
     // Pass in an array of JSON
     // Stock Symbol
@@ -23,4 +33,16 @@ export default function ScrollingStripContainer(){
         <PresentScrollingStrip innerText={text} json={JSON}></PresentScrollingStrip>
     );
     
+}
+
+function getStockById(userId,setStockJson){
+    
+
+    axios.get((url + `/stocks/all/${userId}`))
+    .then((resp) => {
+        setStockJson(resp.data);
+        console.log(resp.data);
+    }, (error) => {
+        console.log(error);
+    });
 }
