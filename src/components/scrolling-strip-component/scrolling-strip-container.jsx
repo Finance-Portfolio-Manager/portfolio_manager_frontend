b07c667f4f27da4331ae016797cbd5eeb498f469
Dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2021-08-08 19:40:04 -0400

    refactored code

--- src/components/scrolling-strip-component/scrolling-strip-container.jsx
+++ src/components/scrolling-strip-component/scrolling-strip-container.jsx
@@ -12,10 +12,7 @@ const url = "http://3.133.113.250:8082/api/get-symbol-pnl"
 
 
 export default function ScrollingStripContainer(){
-    var text = "⬆ ⬇ Just relax and let it flow. That easy. You could sit here for weeks with your one hair brush trying to do that - or you could do it with one stroke with an almighty brush. We can fix anything. It just happens - whether or not you worried about it or tried to plan it. How to paint. That's easy. What to paint. That's much harder. Don't fiddle with it all day.";
 
-    
-        
     // Pass in an array of JSON
     // Stock Symbol
     // PNL
@@ -38,44 +35,27 @@ export default function ScrollingStripContainer(){
     // return(
     //     <PresentScrollingStrip innerText={text} json={JSON}></PresentScrollingStrip>
     // );
-    const JSON = [
-        { symbol: "AMZN" , PNL: -5 },
-        { symbol: "GOOG" , PNL: +5}
-    ]
 
     const [stockJson, setStockJson] = useState([]);
+    const [tickers, setTickers] = useState(["MSFT","GOOG"]);
+
 
     useEffect(()=> {
-        getSymbolPnlFromApi(setStockJson);
-        // getStockById(1,setStockJson); 
-        // returns an array which looks like:
-        // 0: Object {userId: 1, stockId: 1, stockSymbol: "AMZN"}
-        // 1: Object {userId: 1, stockId: 2, stockSymbol: "GOOG"}
+        getSymbolPnlFromApi(tickers, setStockJson);
     },[]);
     
     // let stockVals = Object.values(stockJson);
     return(
-        <ScrollingStripPresentation json={JSON} stockJson={stockJson}></ScrollingStripPresentation>
+        <ScrollingStripPresentation stockJson={stockJson}></ScrollingStripPresentation>
     );
     
 }
 
-// function getStockById(userId,setStockJson){
-
-//     axios.get((url + `/stocks/all/${userId}`))
-//     .then((resp) => {
-//         setStockJson(resp.data);
-//         // console.log(resp.data);
-//     }, (error) => {
-//         console.log(error);
-//     });
-// }
-
-function getSymbolPnlFromApi(setStockJson){
+function getSymbolPnlFromApi(tickers, setStockJson){
     console.log("pinging server!");
     axios.post(
         url,
-        '["MSFT","GOOG"]', //this eventually needs to be passed into the function
+        JSON.stringify(tickers), //this eventually needs to be passed into the function
         {
             headers:{
                 'Content-Type' : 'application/json'
--- src/components/scrolling-strip-component/scrolling-strip-presentation.jsx
+++ src/components/scrolling-strip-component/scrolling-strip-presentation.jsx
@@ -4,27 +4,8 @@ import Marquee from "react-fast-marquee";
 
 export default function ScrollingStripPresentation(props){
 
-    const [json, setJson] = useState([]);
-
-    useEffect(() => { setJson(props.json); } , [] ); 
-
     const [stockProp, setStockProp] = useState([]);
-    const propVals = Object.values(json);
-
     useEffect(() => { setStockProp(props.stockJson); }, []);
-
-
-    // console.log(stockPropArray);
-    // var str = propVals.map((value) => ` ${value.symbol} ${value.PNL}%`);
-//     propVals.map((value) => (
-//         <div style={divStyle}>
-//               <h6 style={hStyle}> {value.symbol}: </h6>
-//               <h6 style={colorId(value.PNL)}> {value.PNL}% </h6>
-//         </div>
-
-//          stockPropArray.map((values) => <h6 style={hStyle}>{values.stockSymbol}:</h6>)
-//     ))
-    // const stockPropArray = Object.entries(stockProp);
     const stockPropJson = Object.keys(stockProp);
 
     const hStyle = {
@@ -34,14 +15,19 @@ export default function ScrollingStripPresentation(props){
         display: "inline"
     }
 
+    // <div>
+    // <h6 style={hStyle}>{x}:{stockPropJson[x]}</h6>
+    
+    // </div>
     return(
         <Marquee style={marqueeStyle} speed="50" gradient={false}> 
         {stockPropJson.map((x) => 
             <div>
                 <h6 style={hStyle}>{x}:</h6>
-                <h6 style={colorId(stockProp[x])}>{stockProp[x]}</h6>
+                <h6 style={hStyle, colorId(stockProp[x])}>{stockProp[x]}</h6>
             </div>
-            )}
+            
+        )}
         {/* use Object.Keys instead so that I can map and output the keys and the values?
             Regardless, look at the keys and entries functions for Object more in-depth */}
         </Marquee> 
@@ -50,7 +36,9 @@ export default function ScrollingStripPresentation(props){
 }
 
 function colorId(pnl){
-    return (pnl < 0) ? {display: "inline", margin: 0, marginRight: 10, color:"red"} : {display: "inline", marginRight: 10, margin: 0, color: "green"};
+    return (pnl < 0) ? 
+            {display: "inline", margin: 0, marginRight: 10, color:"red"} : 
+            {display: "inline", marginRight: 10, margin: 0, color: "green"};
 }
 
 const marqueeStyle = {
