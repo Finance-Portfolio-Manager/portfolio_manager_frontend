4a0e55dfc15d597af6f8e0d8745a0dec3dec96ee
Cody Anderson <anderscody09@gmail.com>
2021-08-10 18:14:27 -0500

    cleaned up router

--- src/components/common/authorization.js
+++ src/components/common/authorization.js
@@ -2,6 +2,23 @@ import { Redirect } from "react-router";
 
 export default function Authorization(props){
 
+    //THIS STATE IS LOST UPON REFRESH, FORCING USER TO RE-LOGIN :(  
+//   const [loggedIn, setLoggedIn] = useState();
+//   useEffect(()=>{
+//     if(sessionStorage.getItem("Authorization")){ 
+//       console.log("has token");
+//       axios.post("http://localhost:8082/verify", JSON.stringify(sessionStorage.getItem("Authorization")), {headers:{'Content-Type': 'application/json'}})
+//       .then(response=>{          
+//         setLoggedIn(true);
+//       })
+//       .catch(function (error) {
+//         setLoggedIn(false);
+//     })
+//     }     
+//     console.log(loggedIn);
+// },[]);
+
+
     let loggedIn = false;
     if(sessionStorage.getItem("Authorization")){
         loggedIn = true;
