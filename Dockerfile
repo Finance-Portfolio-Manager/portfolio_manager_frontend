63c30556fe07d52d22c9338f28d3b8466bce0ba4
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-10 09:43:24 -0700

    not finished

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
-FROM httpd:2.4-alpine
-EXPOSE 80
-COPY ./build/ /usr/local/apache2/htdocs
+FROM nginx:alpine
+EXPOSE 8080
+COPY ./nginx.conf /etc/nginx/conf.d/default.conf
+RUN rm -rf /usr/share/nginx/html/*
+COPY ./build/ /usr/share/nginx/html
+ENTRYPOINT ["nginx", "-g", "daemon off;"]
--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 docker stop finportman-app-dev
 npm run build-dev
-docker build -t dev/finportman-app-dev -f Dockerfile-dev .
+# docker build -t dev/finportman-app-dev -f Dockerfile-dev .
+docker build -t dev/finportman-app .
 docker run \
     -d \
     --rm \
     --network="host" \
-    --name finportman-app-dev \
-    dev/finportman-app-dev
+    --name finportman-app \
+    dev/finportman-app
 docker rmi -f $(docker images -f "dangling=true" -q)
