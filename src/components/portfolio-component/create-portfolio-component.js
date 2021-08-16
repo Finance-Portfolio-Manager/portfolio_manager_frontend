e922eb0e0e79d94d9af400d713ed562b38c90b09
SkySharks <hppyhnd@yahoo.com>
2021-08-16 15:15:00 -0400

    create-portfolio-component.js finished, though unstyled and untested.

--- src/components/portfolio-component/create-portfolio-component.js
+++ src/components/portfolio-component/create-portfolio-component.js
@@ -1,60 +1,60 @@
 import userEvent from "@testing-library/user-event";
-import axios;
+import axios from "axios";
 import React from "react";
 
 export default function CreatePortfolio() {
 
-    return (<>
-    {
-        const URL = "apexstocks.xyz";
+    const URL = "apexstocks.xyz";
 
     /* Sends JWT to get user object from server */
-        function convertJwtToUser() {
-            let user = {};
+    function convertJwtToUser() {
+        let user = {};
 
-            /* TODO get the JWT from session storage */
+        /* TODO get the JWT from session storage */
 
-            axios.get(URL + "/login", JSON.stringify(registrationInfo), {headers:{'Content-Type': 'application/json'}})
-                .then(response=>{
-                    user = response.data;
-                })
-                .catch(function (error) {
-                    if (!error.response) {
-                        console.log("not connected to anything");
-                    }
-                    else if (error.response.status < 200 || error.response.status > 299) {
-                        console.log("Unknown error occurred when resolving JWT to a user object");
-                    }
-                });
+         axios.get(URL + "/login", JSON.stringify(registrationInfo), {headers:{'Content-Type': 'application/json'}})
+            .then(response=>{
+                user = response.data;
+            })
+            .catch(function (error) {
+                if (!error.response) {
+                    console.log("not connected to anything");
+                }
+                else if (error.response.status < 200 || error.response.status > 299) {
+                    console.log("Unknown error occurred when resolving JWT to a user object");
+                }
+            });
 
-            return;
-        };
+        return;
+    };
 
     /* Convert HTML form to JSON */
-        function handleSubmit(event) {            
-            event.preventDefault();
-            const data = new FormData(event.target);
+    function handleSubmit(event) {            
+        event.preventDefault();
+        const data = new FormData(event.target);
 
-            const values = Object.fromEntries(data.entries());
-            document.getElementById('submitForm').submit();
-        };
+        const values = Object.fromEntries(data.entries());
+        document.getElementById('submitForm').submit();
+    };
 
-        document.querySelector('form').addEventListener('submit', handleSubmit);
-    }
+    document.querySelector('form').addEventListener('submit', handleSubmit);
 
-    <form action="http://portfoliomanager-env.eba-49pyjjuv.us-east-2.elasticbeanstalk.com/portfolios">
-        <input name="username" type="hidden" value={user.username}></input>
-        <input name="password" type="hidden" value={user.password}></input>
-        <input name="userId"   type="hidden" value={user.id}></input>
 
-        <label for="name">Portfolio Name</label>
-        <input name="name" type="text" id="name">My Portfolio</input>
+    return (<>
+        <form action="http://portfoliomanager-env.eba-49pyjjuv.us-east-2.elasticbeanstalk.com/portfolios">
+            <input name="username" type="hidden" value={user.username}></input>
+            <input name="password" type="hidden" value={user.password}></input>
+            <input name="userId"   type="hidden" value={user.id}></input>
 
-        <label for="public">Public</label>
-        <input name="public" type="checkbox" id="public" checked></input>
+            <label for="name">Portfolio Name</label>
+            <input name="name" type="text" id="name">My Portfolio</input>
 
-        <input type="button" id="submitButton">Add portfolio</input>
-    </form>
+            <label for="public">Public</label>
+            <input name="public" type="checkbox" id="public" checked></input>
+
+            <input type="button" id="submitButton">Add portfolio</input>
+        </form>
+
     </>);
 
 }
