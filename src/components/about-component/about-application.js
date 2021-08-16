184a71def088ad3f99d285ec7ba6d912e6f72581
RensyAikara <rensy.aikara@gmail.com>
2021-08-16 16:53:42 -0500

    made changes to about page

--- src/components/about-component/about-application.js
+++ src/components/about-component/about-application.js
@@ -2,10 +2,10 @@ import Card from "react-bootstrap/Card";
 
 export default function AboutCard(){
     return (
-        <Card id="about-card" className="title border-0">
+        <Card id="about-card" className="title border-0" data-testid="todo-1">
             <Card.Body>
-                <Card.Title>About</Card.Title>
-                <Card.Text>Portfolio Manager was designed to be a central location for users to track their stock positions. 
+                <Card.Title id="aboutTitle">About</Card.Title>
+                <Card.Text id="aboutPart">Apex Stocks was designed to be a central location for users to track their stock positions. 
                     Allows users to enter their stock purchases and sales, which will be reflected in our portfolio, showing stock 
                     specific and overall gains and losses. Leverages Yahoo Finance API to produce real-time stock details. 
                     Ensured application security by leveraging JSON Web Tokens and partial integration of Spring Security.</Card.Text>
