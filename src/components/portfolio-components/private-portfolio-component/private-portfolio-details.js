07be2202606db7a8fae29db93158cff70ebdbc7f
millerkalvin <Millerkalvin@gmail.com>
2021-08-17 15:06:25 -0400

    Made stock chart appear when you click on it

--- src/components/portfolio-components/private-portfolio-component/private-portfolio-details.js
+++ src/components/portfolio-components/private-portfolio-component/private-portfolio-details.js
@@ -1,8 +1,16 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Modal, Button } from "react-bootstrap";
 import PrivatePortfolioTable from "./private-portfolio-table";
+import ChartComponent from "../../chart-component/chart-component";
 import DoughnutChart from "../doughnut-chart-component/doughnut-chart";
+import { useState } from "react";
 
 export default function PrivatePortfolioDetails(props){
+
+    let [chartSymbol, setChartSymbol] = useState("");
+    let [showStockChart, setShowStockChart] = useState(false);
+
+    let handleCloseChart = () => setShowStockChart(false);
+    let handleOpenChart = () => setShowStockChart(true)
 
     return (
         <>
@@ -10,9 +18,38 @@ export default function PrivatePortfolioDetails(props){
             <h3>{props.portfolio.name}</h3>
         </Row>
         <Row >
+            <Modal 
+                show={showStockChart} 
+                onHide={handleCloseChart}
+                size="lg"
+            >
+                
+                <Modal.Header closeButton>
+                <Modal.Title>{chartSymbol}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <ChartComponent
+                        symbol={chartSymbol}
+                        type="candlestick" 
+                        width="700"
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseChart}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
             <Col md={3} className="folioContent"><DoughnutChart user={props.user} portfolio={props.portfolio}/></Col>
             <Col className="folioContent">
-                <PrivatePortfolioTable tableRows={props.portfolio.stocks} unassignDisplayPortfolio={props.unassignDisplayPortfolio}/>
+
+
+                <PrivatePortfolioTable 
+                    tableRows={props.portfolio.stocks} 
+                    unassignDisplayPortfolio={props.unassignDisplayPortfolio}
+                    onOpenChart={handleOpenChart}
+                    setChartSymbol={setChartSymbol}/>
             </Col>
         </Row>
         </>
