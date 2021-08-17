fd7a13ef6f2358eee4572406a0ef3689fb77fab8
NPBilotta <nick@bilotta.org>
2021-08-17 16:36:24 -0400

    adding button for new transaction

--- src/components/portfolio-components/private-portfolio-component/private-portfolio-view.js
+++ src/components/portfolio-components/private-portfolio-component/private-portfolio-view.js
@@ -1,24 +1,48 @@
 import React from "react";
 import { useState } from "react";
-import PrivatePortfolioDetails from "./private-portfolio-details";
 import PrivatePortfolioCard from "./private-portfolio-card";
+import NewTransaction from "../../new-transaction-component/new_transaction";
 
 export default function PrivatePortfolioView(props){
-
-
 
     function setDisplayPortfolio(){
         props.assignDisplayPortfolio(props.portfolio);
         window.scrollTo(0,0);
     }
 
+    function addTransaction(){
+        return(
+            <div class="container">
+                <NewTransaction portfolio={props.portoflio}/>
+            </div>
+        )
+    }
+
     //Dynamically renders either a portfolio card or a portfolio table component based on whether or not
     //the user clicked the button
     return (
+        <>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                </Button>
+                </Modal.Footer>
+            </Modal>
+
             <PrivatePortfolioCard 
             user={props.user} 
             portfolio={props.portfolio} 
             setDisplayPortfolio={setDisplayPortfolio}
+            addTransaction={addTransaction}
             />
+        </>
     )
 }
