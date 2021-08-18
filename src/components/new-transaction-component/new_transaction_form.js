0bf062bb553c9b3cef5566b70d2a510e207e7fc8
Dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2021-08-18 15:59:01 +0000

    Update new_transaction_form.js

--- src/components/new-transaction-component/new_transaction_form.js
+++ src/components/new-transaction-component/new_transaction_form.js
@@ -1,138 +1,30 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./new_transaction_form.css";
-import Navigation from "../navigation/navigation";
 import { Row, Col } from "react-bootstrap";
-// new-transaction__wholeform
-//new-transaction__form'
-export default function NewTransactionForm(props) {
-  return (
-    // <div className="App__container secondary-color">
-    <Form className="form__style global__blur-effect--light global__dropshadow global__zoom">
-      <div className="new-transaction__container">
-        {/* <Button
-              className="new-transaction__button global__button-effect"
-              name="buy"
-              value="buysell"
-              variant="info"
-              type="button"
-              onClick={props.buyConfirm}
-            >
-              Buy
-            </Button>
-            <Button
-              className="new-transaction__button global__button-effect"
-              name="sell"
-              value="buysell"
-              variant="info"
-              type="button"
-              onClick={props.buyConfirm}
-            >
-              Sell
-            </Button> */}
-      </div>
-      <div className="new-transaction__container primary-text">
-        <Form.Group>
-          <Form.Label>Stock Symbol:</Form.Label>
-          <Form.Control
-            name="stocksymbol"
-            onChange={props.handleChange}
-          ></Form.Control>
-
-          <Form.Label>Quantity:</Form.Label>
-          <Form.Control
-            name="shareAmount"
-            type="number"
-            onChange={props.handleChange}
-          ></Form.Control>
-
-          <Form.Label>Price:</Form.Label>
-          <Form.Control
-            name="sharePrice"
-            type="number"
-            onChange={props.handleChange}
-          ></Form.Control>
-
-          <Form.Label className="m-2">Date:</Form.Label>
-          <Form.Control
-            name="dateTime"
-            type="date"
-            onChange={props.handleChange}
-          ></Form.Control>
-          {/* return (<div class="wholeform"> */}
-          {/* <Form className="m-5 primary-color secondary-text">
-
+export default function NewTransactionForm(props){
+    return (<div class="wholeform">
+        <Form className="m-5 primary-color secondary-text">
             <div class="container">
                 <Form.Group>
                     <Form.Label className="m-2">Stock Symbol:</Form.Label>
                     <Form.Control name="stockSymbol" onChange={props.handleChange}></Form.Control>
-
-              <Form.Label>Date:</Form.Label>
-              <Form.Control
-                name="date"
-                type="date"
-                onChange={props.handleChange}
-              ></Form.Control> */}
-
-          {/* <Form.Label>Note:</Form.Label>
-              <Form.Control
-                name="note"
-                onChange={props.handleChange}
-              ></Form.Control>
-            </Form.Group>
-          </div>
-          <div className="new-transaction__container ">
-            <Button
-              className="new-transaction__button global__button-effect"
-              variant="info"
-              type="submit"
-              onClick={props.submitConfirm}
-            >
-              Add New Transaction
-            </Button>
-            <Button
-              className="new-transaction__button global__button-effect"
-              variant="info"
-              type="submit"
-              onClick={props.getTransaction}
-            >
-              Test Get Transaction
-            </Button>
-          </div> */}
-          {/* <Form.Label  className="m-2">Price:</Form.Label>
-                    <Form.Control name="sharePrice" type="number" onChange={props.handleChange}></Form.Control> */}
-
-          {/* <Form.Label  className="m-2">Date:</Form.Label>
-                    <Form.Control name="dateTime" type="date" onChange={props.handleChange}></Form.Control> */}
-        </Form.Group>
-      </div>
-      <Row>
-        <Col>
-          <Button
-            class="mx-2 secondary-text"
-            name="buy"
-            value="buy"
-            variant="info"
-            type="button"
-            onClick={props.submitBuy}
-          >
-            Buy
-          </Button>
-        </Col>
-        <Col>
-          <Button
-            class="mx-2 secondary-text "
-            name="sell"
-            value="sell"
-            variant="info"
-            type="button"
-            onClick={props.submitSell}
-          >
-            Sell
-          </Button>
-        </Col>
-      </Row>
-    </Form>
-    // </div>
-  );
+                    <Form.Label  className="m-2">Quantity:</Form.Label>
+                    <Form.Control name="transactionQuantity" type="number" onChange={props.handleChange}></Form.Control>
+                    <Form.Label  className="m-2">Price:</Form.Label>
+                    <Form.Control name="sharePrice" type="number" onChange={props.handleChange}></Form.Control>
+                    <Form.Label  className="m-2">Date:</Form.Label>
+                    <Form.Control name="dateTime" type="date" onChange={props.handleChange}></Form.Control>
+                </Form.Group>
+            </div>
+            <Row>
+                <Col>
+                <Button class="mx-2 secondary-text" name = "buy" value = "buy" variant="info" type="button" onClick={props.submitBuy} >Buy</Button>
+                </Col>
+                <Col>
+                <Button class="mx-2 secondary-text" name = "sell" value = "sell" variant="info" type="button" onClick={props.submitSell} >Sell</Button>
+                </Col>
+            </Row>
+        </Form>
+    </div>)
 }
