69fc1955706694f0b3ddf963fb11c6fdf82e6555
Greg McCoy <Gmccoy1344@gmail.com>
2021-08-29 09:43:14 -0500

    Commiting calls

---
+++ src/components/portfolio-component/portfolio-card-component.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Button, Image, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import Portfolio from "./portfolio-component";
+
+//TODO: This image replaces where the chart will eventually be
+import exampleImg from "./exampleimg.svg";
+
+export default function PortfolioCard(props){
+
+
+    const [showDetails, setShowDetails] = useState(true);
+
+    function toggleShowDetails(){
+        setShowDetails(!showDetails)
+    }
+
+
+    //Dynamically renders either a portfolio card or a portfolio table component based on whether or not
+    //the user clicked the button
+    return (
+        <>
+        {showDetails ?
+        <Row>
+            <Col xs={6} md={2}>
+                <Image src={exampleImg} rounded />
+            </Col>
+            <Col>
+                <Card className="center" style={{ width: '50rem', border: '2px solid #F26925'}}>
+                <Card.Img variant="top" />
+                <Card.Body>
+                    <Card.Title>{props.portfolio.name}</Card.Title>
+                    <Card.Text>
+                        Current value: {props.portfolio.value} %change placeholder
+                    </Card.Text>
+                    <Button variant="primary" onClick={toggleShowDetails}>Details</Button>
+                </Card.Body>
+                </Card>
+            </Col>
+            <Col md={2}/>
+        </Row>
+        :
+        <Row>
+            <Col xs={6} md={2}>
+                <Image src={exampleImg} rounded />
+            </Col>
+            <Col>
+                <Portfolio portfolioId={props.portfolio.id} toggleShowDetails={toggleShowDetails}/>
+            </Col>
+            <Col md={2}/>
+        </Row>
+        }
+        </>
+    )
+}
---
+++ src/components/register-component/register-component.js
@@ -0,0 +1,105 @@
+import axios from "axios";
+import { useState } from "react";
+import RegisterForm from "./register-form";
+
+// const URL = "http://localhost:8082";
+const URL = process.env.REACT_APP_API_URL;
+
+export default function Register(props) {
+
+    const [registrationData, setRegistrationData] = useState("");
+
+    // const [nameError, setNameError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [usernameError, setUsernameError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [passwordLengthError, setPasswordLengthError] = useState(false);
+    const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+
+    const handleChange = (e) =>{
+        const {value,name} = e.target;
+
+        setRegistrationData({...registrationData, [name]:value});
+        // setNameError(false);
+        setEmailError(false);
+        setUsernameError(false);
+        setPasswordError(false);
+        setConfirmPasswordError(false);
+        // validateInput(registrationData.firstName, registrationData.lastName, registrationData.username, registrationData.email, registrationData.password, registrationData.confirmPassword);
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        if (validateInput(registrationData.username, registrationData.email, registrationData.password, registrationData.confirmPassword)) {
+            const registrationInfo = {
+                email:`${registrationData.email}`,
+                username:`${registrationData.username}`,
+                password:`${registrationData.password}`,
+                confirmPassword:`${registrationData.confirmPassword}`
+                };
+            axios.post("http://apexstocks.xyz/services/register", JSON.stringify(registrationInfo), {headers:{'Content-Type': 'application/json'}})
+                .then(response=>{
+                    console.log(response.data);
+                    // props.history.push("/");
+                })
+
+                .catch(function (error) {
+                    if(!error.response) {
+                        console.log("not connected to anything");
+                    }
+                    else if (error.response.status < 200 || error.response.status > 299) {
+                        setUsernameError(true);
+                    }
+                })
+
+        }
+    }
+
+    function validateInput(username, email, password, confirmPassword){
+        
+        console.log(registrationData.username);
+        console.log(registrationData.email);
+        console.log(registrationData.password);
+
+        // Forbid any empty values
+        if((username||email||password||confirmPassword)===(null||undefined)){
+            console.log("Some registration fields were left empty.");
+            return false;
+        }
+
+        
+        // just looks for string of the form string@string.string proper validation should be done with a validation link sent to the email address
+        var emailCheck = /\S+@\S+\.\S+/;
+        if(!emailCheck.test(email)){
+            console.log("Invalid email.");
+            // emailError.hidden = false;
+            setEmailError(true);
+            return false;
+        }
+
+        var passCheck = /[\s~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?()\._]/g;
+        if(!passCheck.test(password) || password.length<8){
+            console.log("invalid password.");
+            // passwordError.hidden = false;
+            setPasswordError(true);
+            return false;
+        }
+
+        if (password.length<8){
+            setPasswordLengthError(true);
+            return false;
+        }
+
+        if(password!==confirmPassword){
+            console.log(password);
+            console.log(confirmPassword);
+            console.log("Password confirmation mismatch.");
+            // retypePasswordError.hidden = false;
+            setConfirmPasswordError(true);
+            return false;
+        }
+        console.log("User input valid...");
+        return true;
+    }
+    return <RegisterForm emailError={emailError} usernameError={usernameError} passwordError={passwordError} passwordLengthError={passwordLengthError} confirmPasswordError={confirmPasswordError} onChange={handleChange} onSubmit={handleSubmit}></RegisterForm>
+}
